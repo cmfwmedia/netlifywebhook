0b2ad3eb6ebaea6cdb159eb6774fadf0fe1be27a
cmfwmedia <tim@cmfwmedia.com>
2023-11-07 16:57:54 -0600

    ...

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,16 +1,49 @@
+// import fetch from 'node-fetch'
+
+// export const handler = async () => {
+//   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+//   const response = await fetch(POKE_API)
+//   const data = await response.json()
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       pokemon: data.pokemon_entries
+//     })
+//   }
+// }
+// pokedex.js
+// import fetch from 'node-fetch'
+
+// export const handler = async (event, context) => {
+//   console.log({event}, {context})
+//   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+//   const response = await fetch(POKE_API)
+//   const data = await response.json()
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       pokemon: data.pokemon_entries
+//     })
+//   }
+// }
+
 import fetch from 'node-fetch'
 
+export const handler = async (event, context) => {
+  const eventBody = JSON.parse(event.body)
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
-export const handler = async () => {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
-  
-    const response = await fetch(POKE_API)
-    const data = await response.json()
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        data
-      })
-    }
+  const response = await fetch(POKE_API)
+  const data = await response.json()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries
+    })
   }
+}
