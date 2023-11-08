7c0d5f4e8c646210788da3758894f6cd228a8c7c
cmfwmedia <tim@cmfwmedia.com>
2023-11-08 13:08:17 -0600

    make call to drone deploy api with env variable, cors exceptions

--- netlify/functions/hello-world.mjs
+++ netlify/functions/hello-world.mjs
@@ -7,15 +7,55 @@
 //     }
 //   }
 
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Allow requests from any origin
-      'Access-Control-Allow-Headers': 'Content-Type', // Allow Content-Type header
-    },
-    body: JSON.stringify({
-      message: 'Hello World!',
-    }),
-  };
-};
+import fetch from 'node-fetch'
+
+
+// export const handler = async () => {
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+//       'Access-Control-Allow-Headers': 'Content-Type', // Allow Content-Type header
+//     },
+//     body: JSON.stringify({
+//       message: 'Hello World!',
+//     }),
+//   };
+// };
+
+const handler = async (req, res) => {
+  try {
+    const apiKey = process.env.DRONE_DEPLOY_API_KEY;
+    const url = 'https://www.dronedeploy.com/graphql';
+    const query = JSON.stringify({
+      query: `
+        {
+          viewer {
+            organization {
+              name
+            }
+          }
+        }
+      `
+    });
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE' // Allow the specified methods
+      },
+      body: query
+    });
+
+    const data = await response.json();
+    console.log('API Response:', data);
+    res.set('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.status(200).send('API call successful');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};  
