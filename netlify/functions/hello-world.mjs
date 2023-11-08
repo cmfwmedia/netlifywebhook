3513e42bb80d2d17a5256b36d8849061c91a3e59
cmfwmedia <tim@cmfwmedia.com>
2023-11-08 12:48:52 -0600

    add cors exception

--- netlify/functions/hello-world.mjs
+++ netlify/functions/hello-world.mjs
@@ -1,8 +1,21 @@
+// export const handler = async () => {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         message: 'Hello World!',
+//       }),
+//     }
+//   }
+
 export const handler = async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Hello World!',
-      }),
-    }
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+      'Access-Control-Allow-Headers': 'Content-Type', // Allow Content-Type header
+    },
+    body: JSON.stringify({
+      message: 'Hello World!',
+    }),
+  };
+};
